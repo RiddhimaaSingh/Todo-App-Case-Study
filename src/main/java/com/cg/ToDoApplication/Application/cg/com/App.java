b18f4104a0bb20ebb641b86e37fd352d230702fd
RiddhimaaSingh <118214685+RiddhimaaSingh@users.noreply.github.com>
2025-05-23 10:27:17 +0530

    add

---
+++ src/main/java/com/cg/ToDoApplication/Application/cg/com/App.java
@@ -0,0 +1,13 @@
+package com.cg.ToDoApplication.Application.cg.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
